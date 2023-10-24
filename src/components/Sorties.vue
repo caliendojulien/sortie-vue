0057842e3559aaaf70cd86a4a1b5588f447a30a4
Caliendo Julien <caliendo@hotmail.fr>
2023-10-24 14:25:26 +0200

    Commit initial

---
+++ src/components/Sorties.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+let sorties = ref(null);
+
+fetch("http://10.16.200.2:8000/api/sorties.json")
+    .then(res => res.json())
+    .then(json => sorties.value = json)
+
+</script>
+
+<template>
+  <div v-if="sorties">
+    <div v-for="sortie of sorties">
+      {{ sortie.nom }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Sorties from "../components/Sorties.vue";
+</script>
+
+<template>
+  <main>
+    <Sorties />
+  </main>
+</template>
